1e5965945f07bbb20dffd054306115f07dba7db8
ksafouan <khadijasafouan88@gmail.com>
2023-03-21 06:42:03 -0700

    lower case character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check description
+ * Description: check for loer case character
+ * Return: 1 if lower case 0 if not
+ */
+int _islower(int c)
+{
+	char i;
+	int low = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			low = 1;
+	}
+
+	return (low);
+}
