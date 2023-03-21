be9009ed3c272202d1814e12346aa01b6265fa8e
ksafouan <khadijasafouan88@gmail.com>
2023-03-21 16:43:12 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - check description
+ * description: adds two int
+ * @a: int number1
+ * @b: int number2
+ * Return: result off add 
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
