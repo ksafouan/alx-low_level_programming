f720174aa537efd85f1247734a1bc770b7c4abe8
ksafouan <khadijasafouan88@gmail.com>
2023-03-23 05:57:38 -0700

    m

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,15 @@
  * followed by a new line.
  * Return: Always 0
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i = '0';
 
 	for (; i <= '9'; i++)
 	{
-		if (i != '2' && i != '4')
-			_putchar(i);
+		if (i == '2' || i == '4')
+			i++;
+		_putchar(i);
 	}
 	_putchar('\n');
 }
