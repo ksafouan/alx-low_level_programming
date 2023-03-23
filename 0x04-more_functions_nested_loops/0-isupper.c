da2a903026644a7e63548c10a7b29f0a5a27a2ec
ksafouan <khadijasafouan88@gmail.com>
2023-03-23 05:37:00 -0700

    uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper - check description
+ * description: checks fro uppercase character
+ * @c: input character
+ * Return: 1 if @c is uppercase, 0 if not
+ */
+int _isupper(int c)
+{
+	char upper;
+	int isupper = 0;
+
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		if (c == upper)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+	
+	return (isupper);
+}
