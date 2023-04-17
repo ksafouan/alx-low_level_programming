64e402c73594248f4eb7db723cbb4d3dccd57b3a
ksafouan <khadijasafouan88@gmail.com>
2023-04-17 02:57:39 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for basic dog
+ * @name: dog name
+ * @ages: dog age
+ * @owner: dog owner
+ * Description: data structure for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog *d);
+#endif
