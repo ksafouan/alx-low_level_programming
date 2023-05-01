898fae331fb2eaa0258c262de876fe87baae2568
ksafouan <khadijasafouan88@gmail.com>
2023-05-01 10:50:35 -0700

    new changes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,40 +1,49 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function that adds node at the index
- * @head: pointer of the head list
- * @idx: the given index
- * @n: input int
- * Return: address of new node or NULL if it failed
+ * insert_nodeint_at_index - A function that inserts node at given index
+ * @head: A pointer to listint_t structure
+ * @idx: The index of the list
+ * @n: An integer data for new node
+ * Return: The address to new node at specified index, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node, *new_node = malloc(sizeof(listint_t));
-	unsigned int i = 0;
+	unsigned int i = 1;
+	listint_t *new_node, *temp;
 
-	if (!head || !new_node)
+	if (head == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
-	if (!idx)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		new_node->n = n;
+		return (new_node);
+	}
+
+	if (idx == 0)
 	{
 		new_node->next = *head;
+		new_node->n = n;
 		*head = new_node;
 		return (new_node);
 	}
-	node = *head;
-	while (node)
+
+	temp = *head;
+	while (i < idx)
 	{
-		if (i == idx - 1)
-		{
-			new_node->next = node;
-			node = new_node;
-			return (new_node);
-		}
+		temp = temp->next;
 		i++;
-		node = node->next;
 	}
-	free(new_node);
-	return (NULL);
+
+	new_node->n = n;
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
---
+++ 0x13-more_singly_linked_lists/new_insert.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function that adds node at the index
+ * @head: pointer of the head list
+ * @idx: the given index
+ * @n: input int
+ * Return: address of new node or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *new_node = malloc(sizeof(listint_t));
+	unsigned int i = 0;
+
+	if (!head || !new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == idx - 1)
+		{
+			new_node->next = node;
+			node = new_node;
+			return (new_node);
+		}
+		i++;
+		node = node->next;
+	}
+	free(new_node);
+	return (NULL);
+}
