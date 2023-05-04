7b6e5cea08743d387f0a431482d708e1bb454a91
ksafouan <khadijasafouan88@gmail.com>
2023-05-04 13:21:32 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_binary - function that print binary
+ * @n: input usigned long int
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int bit = sizeof(n) * 8, print = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			print++;
+		}
+		else if (print)
+			_putchar('0');
+	}
+
+	if (!print)
+		_putchar('0');
+}
