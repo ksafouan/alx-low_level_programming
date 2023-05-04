81c528bf0cc284a25ea85b0f01e934edfec92c38
ksafouan <khadijasafouan88@gmail.com>
2023-05-04 13:14:39 -0700

    convert binary to unsigne int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,8 +10,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int n = 0;
 
 	if (!b)
-		return (NULL);
-	while (b)
+		return (0);
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
