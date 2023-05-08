ccb6fc4acb07248a89d19e64586ab6e1edb569c2
ksafouan <khadijasafouan88@gmail.com>
2023-05-08 13:51:42 -0700

    get to header ELF

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - main function to get header ELF
+ * @argc: count arguemnt
+ * @argv: vector argument
+ * Return: 1 in success, ERROR in failure
+ */
+int main(int argc, char *argv[])
+{
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
+}
